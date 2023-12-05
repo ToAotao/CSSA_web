cc491653a14dd6bdb6f1b94c4638bea20e84c131
ToAotao <xiaotaowu345@gmail.com>
2023-12-04 21:51:39 -0500

    first commit

---
+++ src/main/java/com/cssa/web/OfficalWebApplication.java
@@ -0,0 +1,13 @@
+package com.cssa.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficalWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cssa/web/OfficalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssa.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
